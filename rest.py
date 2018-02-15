fd2f82c89b06eba94ab7dded2620b417b906a510
Edward Luo <eluo1@stuy.edu>
2018-02-15 00:30:40 -0500

    worked on hw

--- rest.py
+++ rest.py
@@ -10,7 +10,14 @@ def borough( b ) :
     return collection.find( {'borough' : b } )
 
 def zipcode( z ) :
-    return collection.find( { 'address.zipcode' : z } } )
+    return collection.find( { 'address.zipcode' : z } )
+
+def qualityZip ( z, grade ) :
+    return collection.find( { 'addres.zipcode' : z }, { 'grades.grade' : grade } )
+
+def lowScore ( z, score ) :
+    return collection.find( { 'address.zipcode' : z }, 'grades.score' : { '#lt' : score } )
+
 
 test = zipcode( '10451' )
 for restaurant in test:
